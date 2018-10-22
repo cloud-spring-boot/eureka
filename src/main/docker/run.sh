9ac241e5241950c5f11ea97b3cfa56df52d92ee6
Maksym Stepanenko <maksym.stepanenko@opower.com>
2018-10-22 17:50:44 +0300

    tcpdump added

--- src/main/docker/run.sh
+++ src/main/docker/run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+nohup tcpdump -A -s 0 'tcp port 8761 and (((ip[2:2] - ((ip[0]&0xf)<<2)) - ((tcp[12]&0xf0)>>2)) != 0)' \
+-w /usr/local/eureka/pcap/eureka.pcap &
+
 echo "********************************************************"
 echo "Starting eureka "
 echo "********************************************************"
